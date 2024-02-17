45d29c8d0d7116500557f456025e775d3c229e0e
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-02-18 03:51:34 +0900

    WezTermの簡単な設定を追加

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font('HackGen Console NF')
+config.font_size = 18 
+config.color_scheme = 'Catppuccin Macchiato'
+config.adjust_window_size_when_changing_font_size = false
+config.window_decorations = 'RESIZE'
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.95
+config.text_background_opacity = 0.95
+config.macos_window_background_blur = 15
+config.audible_bell = "Disabled"
+
+return config
