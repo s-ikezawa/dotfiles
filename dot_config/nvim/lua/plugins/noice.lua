423e1884dd95c6570becb006a398a1bb8be365f6
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-02-21 02:05:46 +0900

    optsに書き直し

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -12,11 +12,9 @@ return {
 		--   If not available, we use `mini` as the fallback
 		{
       "rcarriga/nvim-notify",
-      config = function()
-        require("notify").setup({
-          background_colour = "#000000",
-        })
-      end
+      opts = {
+        background_colour = "#000000",
+      },
     },
 	},
 	config = function()
