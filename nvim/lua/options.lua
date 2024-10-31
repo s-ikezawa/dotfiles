c09e75adb2c2347d3802dcd875c321bbe1e7535f
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-10-31 19:31:59 +0900

    インデントの設定をした

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,3 +5,12 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- indention
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
