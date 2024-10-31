dd75933f4e0842f375f48865d87c36fa36396b7c
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-10-31 21:10:55 +0900

    statuscol.nvimをインストールした

---
+++ nvim/lua/plugins/statuscol.lua
@@ -0,0 +1,35 @@
+return {
+  "luukvbaal/statuscol.nvim",
+  event = "VeryLazy",
+  config = function()
+    local builtin = require("statuscol.builtin")
+    require("statuscol").setup({
+      bt_ignore = {
+        "terminal",
+        "nofile",
+        "ddu-ff",
+        "ddu-ff-filter",
+      },
+      relculright = true,
+      segments = {
+        { -- Diagnosticの表示
+          sign = {
+            name = { "Diagnostic.*" },
+            maxwidth = 1,
+          },
+        },
+        { -- 行番号の表示
+          text = { builtin.lnumfunc },
+        },
+        { -- gitsignsの表示
+          sign = {
+            namespace = { "gitsigns" },
+            maxwidth = 1,
+            colwidth = 1,
+            wrap = true,
+          },
+        },
+      },
+    })
+  end
+}
