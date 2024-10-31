bd4a0289c7e7096c1ff60c5464c38a8e6b94e275
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-10-31 15:46:05 +0900

    karabiner-elementsのインストールと設定ファイルのlink

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ export XDG_DATA_HOME=$HOME/.local/share
 # Make directories
 mkdir -p $XDG_CONFIG_HOME
 mkdir -p $XDG_CACHE_HOME
-mkdir -p $XDG_DATA_HOME
+mkdir -p $XDG_DATA_HOME/zsh # zsh用のディレクトリを事前に作成しておく
 
 # Homebrew
 if !(type brew > /dev/null 2>&1); then
@@ -29,5 +29,14 @@ brew bundle --file=./Brewfile
 # /etc/zshenvはMacの場合なので他のOSの場合は要確認
 echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee /etc/zshenv
 
+################################################################################
 # link
+################################################################################
+# zsh
 ln -sf $REPO_DIR/zsh $HOME/.config/zsh
+
+# karabiner-elements
+if [ ! -d $HOME/.config/karabiner ]; then
+  mkdir -p $HOME/.config/karabiner
+fi
+ln -sf $REPO_DIR/karabiner/karabiner.json $HOME/.config/karabiner/karabiner.json
