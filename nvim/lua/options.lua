6f5dfde452007473fafcd3cf8e71f897cfde9c8c
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-10-31 19:42:36 +0900

    見た目の設定を入れた

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,11 +2,18 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = ''
+vim.opt.mouse = ""
 
 -- ui
+vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.showmode = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.cursorline = true
 
 -- indention
 vim.opt.expandtab = true
