b68f0bee8e0369ca0ea48f3f05ece9e8c0076ef5
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-10-31 20:57:51 +0900

    gitsigns.nvimをインストールした

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,65 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("gitsigns").setup({
+      signs = {
+        delete = { text = "󰍵" },
+        changedelete = { text = "󱕖" },
+      },
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gitsigns.nav_hunk("next")
+          end
+        end, { desc = "Jump to next git change" })
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gitsigns.nav_hunk("prev")
+          end
+        end, { desc = "Jump to previous git change" })
+
+        -- Actions
+        -- visual mode
+        map("v", "<leader>hs", function()
+          gitsigns.stage_hunk { vim.fn.line("."), vim.fn.line("v") }
+        end, { desc = "stage git hunk" })
+        map("v", "<leader>hr", function()
+          gitsigns.reset_hunk { vim.fn.line("."), vim.fn.line("v") }
+        end, { desc = "reset git hunk" })
+
+        -- normal mode
+        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "git stage hunk" })
+        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "git reset hunk" })
+        map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "git stage buffer" })
+        map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "git undo stage hunk" })
+        map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "git reset buffer" })
+        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "git preview hunk" })
+        map("n", "<leader>hd", gitsigns.diffthis, { desc = "git diff against index" })
+        map("n", "<leader>hD", function()
+          gitsigns.diffthis('@')
+        end, { desc = "git diff against last commit" })
+
+        -- toggle
+        map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle git show blame line" })
+        map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Toggle git show deleted" })
+
+        -- Text object
+        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      end
+    })
+  end,
+}
