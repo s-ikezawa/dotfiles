36dd0057136d7590ade18183b5318415a05aef6f
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-11-04 18:23:53 +0900

    vscodeのユーザ設定ファイルのsymlinkを作成した

--- link.sh
+++ link.sh
@@ -1,4 +1,10 @@
 XDG_CONFIG_HOME=$HOME/.config
 REPO_DIR=$HOME/Projects/github.com/s-ikezawa/dotfiles
 
+# zsh
 ln -snfv $REPO_DIR/zsh $XDG_CONFIG_HOME/zsh
+
+# vscode
+ln -snfv $REPO_DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -snfv $REPO_DIR/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets
+ln -snfv $REPO_DIR/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
--- unlink.sh
+++ unlink.sh
@@ -1,3 +1,6 @@
 XDG_CONFIG_HOME=$HOME/.config
 
 unlink $XDG_CONFIG_HOME/zsh
+unlink $HOME/Library/Application\ Support/Code/User/settings.json
+unlink $HOME/Library/Application\ Support/Code/User/snippets
+unlink $HOME/Library/Application\ Support/Code/User/keybindings.json
