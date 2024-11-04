21ccabc19c3413b13f9eff714580b40d70f02a39
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-11-04 18:51:27 +0900

    vscode neovimをインストールした

--- link.sh
+++ link.sh
@@ -8,3 +8,6 @@ ln -snfv $REPO_DIR/zsh $XDG_CONFIG_HOME/zsh
 ln -snfv $REPO_DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 ln -snfv $REPO_DIR/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets
 ln -snfv $REPO_DIR/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+
+# neovim
+ln -snfv $REPO_DIR/nvim $XDG_CONFIG_HOME/nvim
---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+if vim.g.vscode then
+    require "visual-studio-code"
+else
+    require "mynvim"
+end
