fbbfcd0b48b6260f066a8b88a847efd1a4d8876c
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-12-19 19:49:36 +0900

    weztermの設定ファイルを追加

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("HackGen Console NF")
+config.font_size = 16.0
+
+return config
