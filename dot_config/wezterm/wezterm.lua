6979932d1a954f261b2f173124c3b8e23d4f120f
Shinichi Ikezawa <shinichi@ikezawa.tech>
2024-12-19 19:51:30 +0900

    weztermのcolor schemeを変更した

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("HackGen Console NF")
 config.font_size = 16.0
 
