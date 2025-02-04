6bfdc5e2a9eb71367617fb191336f87b53fb4062
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-02-04 17:46:52 +0900

    mini.iconをインストールした

---
+++ dot_config/nvim/lua/custom/plugins/mini.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    lazy = true,
+    opts = {
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      },
+      filetype = {
+        dotenv = { glyph = "", hl = "MiniIconsYellow" },
+      },
+    },
+  },
+}
