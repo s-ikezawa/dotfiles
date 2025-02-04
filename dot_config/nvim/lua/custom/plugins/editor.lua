7aeca82b4eee61f64323e541cfe2e9455376a760
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-02-04 18:43:12 +0900

    which-keyをインストールした

---
+++ dot_config/nvim/lua/custom/plugins/editor.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VimEnter",
+    opts = {
+      -- classic window下部一杯に表示される
+      -- modern window下部両端のスペースありで表示される
+      -- helix window右下に表示される
+      preset = "helix", 
+      spec = {
+        {
+          mode = { "n", "v" },
+          { "<leader>f", group = "file/find", },
+        },
+      },
+    },
+  },
+}
