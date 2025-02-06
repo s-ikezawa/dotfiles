76f41800f751e8e64d5236898a1b94a67544a714
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-02-06 16:38:29 +0900

    tabなどの不可視文字を可視化する設定を追加した

--- dot_config/nvim/lua/custom/options.lua
+++ dot_config/nvim/lua/custom/options.lua
@@ -4,3 +4,9 @@ vim.opt.number = true
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- 不可視文字の表示
+vim.opt.listchars = { tab = "→ ", trail = "•" }
+vim.opt.list = true
