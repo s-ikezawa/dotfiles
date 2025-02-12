c44a16884dc74b4124f81e50d76af9f3e2fe1290
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-02-12 23:01:23 +0900

    keymap定義用のファイルを作成した

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,4 +2,5 @@ if vim.g.vscode then
 else
   require("custom.lazy")
   require("custom.options")
+  require("custom.keymaps")
 end
---
+++ dot_config/nvim/lua/custom/keymaps.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, desc = "Escape" })
