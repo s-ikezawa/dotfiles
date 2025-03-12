6029fb46730d504cbcf3502173b014395a56d8bd
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-03-12 15:08:42 +0900

    Ghosttyの最低限の設定を入れた

--- scripts/install/deploy.sh
+++ scripts/install/deploy.sh
@@ -2,10 +2,17 @@
 
 set -e
 
+# Zsh
 if ! [ -d ${HOME}/.config/zsh ]; then
   mkdir -p ${HOME}/.config/zsh;
 fi
 stow -d ./config -t ${HOME}/.config/zsh zsh;
+
+# Ghostty
+if ! [ -d ${HOME}/.config/ghostty ]; then
+  mkdir -p ${HOME}/.config/ghostty;
+fi
+stow -d ./config -t ${HOME}/.config/ghostty ghostty;
 
 expect -c "\
   spawn -noecho bash -c \"echo \'export ZDOTDIR=~/.config/zsh' | sudo tee /etc/zshenv\"; \
