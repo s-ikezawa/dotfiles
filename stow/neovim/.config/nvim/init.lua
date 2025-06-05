7dfef0098db6962a34eca27c053c46ab10afe2ec
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-06-05 20:03:37 +0900

    Neovimにlazy.nvimプラグインマネージャーを導入
    - lazy.nvimのbootstrapコードをinit.luaに追加
    - Catppuccin (Mocha)カラースキームを設定
    - Treesitterによる高度なシンタックスハイライトを追加
    - lazy-lock.jsonを.gitignoreに追加（自動生成ファイルのため）

--- stow/neovim/.config/nvim/init.lua
+++ stow/neovim/.config/nvim/init.lua
@@ -4,6 +4,48 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- lazy.nvimのbootstrap
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- プラグインの設定を読み込む
+require('lazy').setup('plugins', {
+  defaults = {
+    lazy = true, -- デフォルトで遅延読み込みを有効化
+  },
+  install = {
+    colorscheme = { 'habamax' }, -- インストール中のカラースキーム
+  },
+  checker = {
+    enabled = true, -- 自動アップデートチェックを有効化
+    notify = false, -- アップデート通知を無効化
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
+
 -- 基本オプション設定
 local opt = vim.opt
 
---
+++ stow/neovim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,153 @@
+-- プラグイン設定のエントリーポイント
+-- このファイルは lazy.nvim によって自動的に読み込まれます
+
+return {
+  -- カラースキーム: Catppuccin
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false, -- カラースキームは即座に読み込む
+    priority = 1000, -- 他のプラグインより先に読み込む
+    config = function()
+      require('catppuccin').setup({
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        background = {
+          light = 'latte',
+          dark = 'mocha',
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        term_colors = true,
+        dim_inactive = {
+          enabled = false,
+          shade = 'dark',
+          percentage = 0.15,
+        },
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+        styles = {
+          comments = { 'italic' },
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        integrations = {
+          nvimtree = true,
+          treesitter = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { 'italic' },
+              hints = { 'italic' },
+              warnings = { 'italic' },
+              information = { 'italic' },
+            },
+            underlines = {
+              errors = { 'underline' },
+              hints = { 'underline' },
+              warnings = { 'underline' },
+              information = { 'underline' },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+        },
+      })
+      -- カラースキームを適用
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
+
+  -- Treesitter: シンタックスハイライト
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = { 'BufReadPost', 'BufNewFile' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        -- インストールするパーサー
+        ensure_installed = {
+          'bash',
+          'c',
+          'cpp',
+          'css',
+          'dockerfile',
+          'go',
+          'html',
+          'javascript',
+          'json',
+          'lua',
+          'markdown',
+          'markdown_inline',
+          'python',
+          'rust',
+          'toml',
+          'tsx',
+          'typescript',
+          'vim',
+          'vimdoc',
+          'yaml',
+        },
+        -- 自動インストールを有効化
+        auto_install = true,
+        -- シンタックスハイライトを有効化
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        -- インデントを有効化
+        indent = {
+          enable = true,
+        },
+        -- テキストオブジェクトを有効化
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+        },
+      })
+    end,
+  },
+}
