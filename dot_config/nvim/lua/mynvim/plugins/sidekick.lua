1283fb5833b28a87fa55dabe2a94dfaecb39f732
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-10-26 10:20:51 +0900

    feat: ClaudeCode用にsidekick.nvimをインストール

---
+++ dot_config/nvim/lua/mynvim/plugins/sidekick.lua
@@ -0,0 +1,35 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    cli = {
+      tools = {
+        claude = {
+          cmd = { "claude" }
+        },
+      },
+      win = {
+        layout = "right",
+        split = {
+          width = 80,
+          height = 0,
+        },
+        keys = {
+          prompt = false, -- <C-p>をCLIに渡す(デフォルトのプロンプト選択機能を無効化)
+        },
+      },
+    },
+    nes = {
+      enabled = false
+    },
+  },
+  keys = {
+    {
+      "<leader>cc",
+      function()
+        require("sidekick.cli").toggle("claude")
+      end,
+      desc = "ClaudeCodeをトグル",
+      mode = { "n", "t", "i", "x" },
+    },
+  },
+}
