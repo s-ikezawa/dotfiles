e786aa174f449f949b0ed792b77b6609e471fc33
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-10-30 12:50:09 +0900

    feat: terminalを有効化

--- dot_config/nvim/lua/mynvim/plugins/snacks.lua
+++ dot_config/nvim/lua/mynvim/plugins/snacks.lua
@@ -6,7 +6,27 @@ return {
   opts = {
     explorer = { enabled = true },
     lazygit = { enabled = true },
-    terminal = { enabled = true },
+    terminal = {
+      enabled = true,
+      win = {
+        position = "bottom", -- デフォルト: "bottom" (コマンドなし) または "float" (コマンドあり)
+        height = 0.2,        -- デフォルト: 0.4 (画面の40%), left or right = 0(full screen)
+        width = 0,           -- デフォルト: bottom = 0(full screen), left or right = 0.4
+      },
+    },
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              width = 30,        -- デフォルト: 40
+              min_width = 30,    -- デフォルト: 40
+              position = "left", -- デフォルト: "left" (または "right")
+            },
+          },
+        },
+      },
+    },
   },
   keys = {
     { "<leader>fe", function() require("snacks").explorer.open() end, desc = "ファイルツリーをトグル" },
