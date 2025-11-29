8d76139641d9ce782b2e7f1707c1c7ba6968bf6a
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-11-29 20:51:43 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,7 +2,9 @@
 -- Options
 --=========================================================================
 if not vim.g.vscode then
+  -- 行番号を相対行番号で表示
   vim.opt.number = true
+  vim.opt.relativenumber = true
 end
 
 -- クリップボード連携
