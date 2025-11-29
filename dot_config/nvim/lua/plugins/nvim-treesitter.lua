d4aac950022f4e00bdae8bd5c7656f6dd14a6080
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-11-29 21:43:41 +0900

    Update .chezmoidata/homebrew.yml Add .config/nvim/lua/plugins/nvim-treesitter.lua

---
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,36 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  branch = 'main',
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter').setup({
+      install_dir = vim.fn.stdpath('data') .. '/site'
+    })
+
+    local types = {
+      'c',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'vim',
+      'vimdoc',
+    }
+
+    require('nvim-treesitter').install(types)
+
+    -- Highlighting
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = types,
+      callback = function() vim.treesitter.start() end,
+    })
+
+    -- Folds
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+    -- Indentation
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end
+}
