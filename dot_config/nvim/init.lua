a68bb57e375b3fc2052b532823c61662b4ad4623
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-11-29 23:46:31 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -49,6 +49,10 @@ require("lazy").setup({
   spec = {
     { import = "plugins" }
   },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  }
 })
 
 --=========================================================================
