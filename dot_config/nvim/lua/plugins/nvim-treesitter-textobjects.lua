3dc00c203f0d17e793377619c2d29623abaa802f
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-11-30 13:51:05 +0900

    Update .config/nvim/lua/plugins/catppuccin.lua Update .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua

--- dot_config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,5 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter-textobjects',
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter', branch = 'main' },
+  },
   branch = 'main',
   keys = {
     {
