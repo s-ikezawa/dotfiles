a58823c7cbd7ef23cb3c67e885a35153c8eeb241
Shinichi Ikezawa <shinichi@ikezawa.tech>
2025-12-13 14:44:43 +0900

    Update .config/mise/mise.toml Update .config/nvim/lua/plugins/snacks.lua Update Library/Application Support/Code/User/settings.json

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -13,3 +13,16 @@ Snacks.setup({
 vim.keymap.set('n', '<leader>fb', function() Snacks.picker.buffers() end)
 vim.keymap.set('n', '<leader>fe', function() Snacks.explorer() end)
 
+-- Terminal
+vim.keymap.set('n', '<leader>tb', function()
+  Snacks.terminal.open(
+    nil,
+    {
+      win = {
+        style = 'terminal',
+        position = 'bottom',
+        height = 0.2,
+        width = 0
+      }
+    }
+) end)
