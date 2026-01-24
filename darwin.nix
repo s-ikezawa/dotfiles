63baba4db0f38bdb604ee56533abdf366d466135
Shinichi Ikezawa <shinichi@ikezawa.tech>
2026-01-24 17:44:46 +0900

    feat: homebrewを使って 1password をインストール

--- darwin.nix
+++ darwin.nix
@@ -1,8 +1,21 @@
 { pkgs, username, ... }: {
   system.stateVersion = 5;
+  system.primaryUser = username;
 
   users.users.${username} = {
     name = username;
     home = "/Users/${username}";
+  };
+
+  homebrew = {
+    enable = true;
+    onActivation = {
+      cleanup = "zap";
+      autoUpdate = true;
+    };
+    casks = [
+      "1password"
+      "1password-cli"
+    ];
   };
 }
