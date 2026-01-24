36160e916b3b2326e93f19dc091d4dca6dc7b58b
Shinichi Ikezawa <shinichi@ikezawa.tech>
2026-01-24 18:03:43 +0900

    feat: Dockは左側で最小サイズに設定し、拡大機能を有効化

--- darwin.nix
+++ darwin.nix
@@ -7,6 +7,17 @@
     home = "/Users/${username}";
   };
 
+  system.defaults = {
+    dock = {
+      autohide = true;      # 自動的に隠す
+      show-recents = false; # 最近使ったアプリは非表示
+      tilesize = 16;        # アイコンサイズ
+      magnification = true; # 拡大を有効化
+      largesize = 128;      # 拡大時のサイズ(max:128)
+      orientation = "left"; # 表示位置(bottom, left, right)
+    };
+  };
+
   homebrew = {
     enable = true;
     onActivation = {
