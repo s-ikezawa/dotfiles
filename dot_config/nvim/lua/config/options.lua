8fbd2b4ab86ac7f689b10ff288970d79eef940de
Shinichi Ikezawa <shinichi@ikezawa.tech>
2026-02-04 18:17:05 +0900

    feat: neovimにcatppuccin(カラースキーマ)をインストール

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.mapleader = ","
+
+vim.opt.termguicolors = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
