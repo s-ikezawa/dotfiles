81a564886e4c088f6cd16e26336091f0f36a175d
Shinichi Ikezawa <shinichi@ikezawa.tech>
2026-02-28 00:18:57 +0900

    sidekick.nvimを導入しターミナルモードのキーマップを追加

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -9,6 +9,21 @@ Snacks.setup({
     enabled = true,
   },
   picker = {
+    actions = {
+      sidekick_send = function(...)
+        return require("sidekick.cli.picker.snacks").send(...)
+      end,
+    },
+    win = {
+      input = {
+        keys = {
+          ["<a-a>"] = {
+            "sidekick_send",
+            mode = { "n", "i" },
+          },
+        },
+      },
+    },
     sources = {
       explorer = {
         hidden = true,
