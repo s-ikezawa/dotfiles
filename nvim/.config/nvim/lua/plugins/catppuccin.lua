d5aa7511ff08e5e6cf7a48915688d5f384909c63
Shinichi Ikezawa <shinichi@ikezawa.tech>
2026-02-27 18:41:18 +0900

    Catppuccinの背景を透過にしてtmuxの非アクティブペイン暗転をNeovimでも有効化

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -11,7 +11,7 @@ require("catppuccin").setup({
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = false, -- disables setting the background color.
+    transparent_background = true, -- tmuxの非アクティブペイン暗転を透過させる
     float = {
         transparent = false, -- enable transparent floating windows
         solid = false, -- use solid styling for floating windows, see |winborder|
