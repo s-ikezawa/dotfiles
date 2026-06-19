ad940e292e0b4acc381eb3055623ce031d273076
Shinichi Ikezawa <shinichi@ikezawa.tech>
2026-06-19 15:17:28 +0900

    fix

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,5 +2,6 @@
 -- Neovim エントリーポイント
 -- ============================================================================
 
-require("options") -- オプション設定（lua/options.lua）を読み込む
-require("plugins") -- プラグイン設定（lua/plugins/init.lua）を読み込む
+require("options")    -- オプション設定（lua/options.lua）を読み込む
+require("autoreload") -- 外部変更の自動取り込み（lua/autoreload.lua）を読み込む
+require("plugins")    -- プラグイン設定（lua/plugins/init.lua）を読み込む
